b3e8e78ef7239c3a85e06527c517a12f4eb2f129
ben_ten_11 <bentenomniverse11@pm.me>
2025-07-13 23:58:28 -0700

    still learning;

--- journey/setup.py
+++ journey/setup.py
@@ -1,21 +1,25 @@
 
 from textual.app import App, ComposeResult
 from textual.containers import HorizontalGroup, HorizontalScroll, VerticalGroup, VerticalScroll
-from textual.widgets import Button, Digits, Footer, Header , Label, Tree
+from textual.widgets import Button, Digits, Footer, Header , Label, Tree, Label
+from rich.text import Text
 
 
 class GoalMenu(VerticalGroup):
     """The main goals area tree"""
 
-    def main_goals_tree(self):
-        tree: Tree[str] = Tree("Main Goals", id="goals_tree")
+    def insert_new_goalTree(self, tree_label: str, tree_id: str):
+        tree: Tree[str] = Tree(tree_label, id=tree_id)
         tree.root.expand()
         main_goals = tree.root.add("Testing", expand=True)
+        tree.root.add_leaf('SUB GOALS')
         [main_goals.add_leaf(str(i)) for i in range(8)]
         return tree
 
     def compose(self) -> ComposeResult:
-        yield Tree("")
+        yield Label(Text('Goals', style="bold red underline", justify='center'))
+        yield self.insert_new_goalTree("Main Goals", "main_goals")
+        # yield self.insert_new_goalTree("Sub Goals", "sub_goals")
 
 
 
